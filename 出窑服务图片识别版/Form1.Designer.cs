06be74b991973a65df007b24a0e1f302f0297091
jie lin <532367171@qq.com>
2019-03-30 22:35:26 +0800

    1-1

--- 出窑服务图片识别版/Form1.Designer.cs
+++ 出窑服务图片识别版/Form1.Designer.cs
@@ -36,15 +36,15 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 40);
+            this.button1.Size = new System.Drawing.Size(52, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "启动";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 58);
+            this.button2.Location = new System.Drawing.Point(40, 58);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(68, 30);
             this.button2.TabIndex = 1;
